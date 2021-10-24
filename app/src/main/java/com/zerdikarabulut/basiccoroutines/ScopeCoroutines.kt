63bd9ae89a67ace746e92febee2a34b8e277c5cd
Erdi Karabulut <tpekarabulut@architecht.com>
2021-10-24 16:52:59 +0300

    Initial commit

---
+++ app/src/main/java/com/zerdikarabulut/basiccoroutines/ScopeCoroutines.kt
@@ -0,0 +1,20 @@
+package com.zerdikarabulut.basiccoroutines
+
+import kotlinx.coroutines.*
+fun main() {
+    runBlocking {
+
+        launch {
+            delay(5000)
+            println("run blocking")
+        }
+
+        coroutineScope {
+            launch {
+                delay(3000)
+                println("coroutine scope")
+            }
+        }
+
+    }
+}
